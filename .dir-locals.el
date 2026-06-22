649713ebc18c53ccb29dd7d09737d0b81791b8d6
Jonas Rodrigues <313741+jxonas@users.noreply.github.com>
2026-06-22 00:26:59 +0000

    chore(tooling): add cljfmt config and dir-locals
    Add .cljfmt.edn (sort ns references, inner-form indents) and .dir-locals.el (CIDER aliases :test:examples, always-indent, clojure-cli build tool).

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((clojure-mode
+  . ((cider-clojure-cli-aliases        . ":test:examples")
+     (clojure-indent-style             . :always-indent)
+     (cider-preferred-build-tool       . clojure-cli))))
